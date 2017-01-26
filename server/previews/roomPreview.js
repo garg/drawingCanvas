14586fa32e4bbc74388c0e08cb831c0a33cf0f5a
garg <khurrum1@gmail.com>
2017-01-25 21:31:08 -0500

    Update roomPreview.js

--- server/previews/roomPreview.js
+++ server/previews/roomPreview.js
@@ -1,12 +1,4 @@
 var noimage = new Buffer(Assets.getBinary('noimage.png'));
 
-//RoomPreviewController = RouteController.extend({action: function() {
-//
-//  this.response.writeHead(404, {'Content-Type': 'image/png',});
-//  this.response.write(noimage);
-//  this.response.end();
-//
-//}});
-
 
 
